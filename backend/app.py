783cbee2bf9bc85a759aae3c3245601077737970
Van Brantley <van.brantley@gmail.com>
2023-11-07 19:50:40 -0500

    Add games route to backend

--- backend/app.py
+++ backend/app.py
@@ -20,13 +20,14 @@ DB_NAME = os.getenv("DB_NAME")
 
 # dialect+driver://username:password@host:port/database
 DB_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+
 class Team(db.Model):
-    __tablename__ = 'teams'
+    __tablename__ = "teams"
     team_id = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String(50), nullable=False)
     team_name = db.Column(db.String(50), nullable=False)
@@ -40,18 +41,25 @@ class Team(db.Model):
     tertiary_color = db.Column(db.String(7), nullable=True)
 
     # relationships
-    players = relationship('Player', back_populates='team')
-    home_games = relationship('Game', foreign_keys='Game.home_team_abbreviation', back_populates='home_team')
-    away_games = relationship('Game', foreign_keys='Game.away_team_abbreviation', back_populates='away_team')
+    players = relationship("Player", back_populates="team")
+    home_games = relationship(
+        "Game", foreign_keys="Game.home_team_abbreviation", back_populates="home_team"
+    )
+    away_games = relationship(
+        "Game", foreign_keys="Game.away_team_abbreviation", back_populates="away_team"
+    )
 
     def __repr__(self):
-        return f'<Team {self.abbreviation}>'
-    
+        return f"<Team {self.abbreviation}>"
+
+
 class Player(db.Model):
-    __tablename__ = 'players'
+    __tablename__ = "players"
     player_id = db.Column(db.Integer, primary_key=True)
     player_name = db.Column(db.String(100), nullable=False)
-    team_abbreviation = db.Column(db.String(3), db.ForeignKey('teams.abbreviation'), nullable=False)
+    team_abbreviation = db.Column(
+        db.String(3), db.ForeignKey("teams.abbreviation"), nullable=False
+    )
     position = db.Column(db.String(3), nullable=False)
     jersey_number = db.Column(db.Integer, nullable=True)
     image_url = db.Column(db.String(100), nullable=True)
@@ -61,40 +69,52 @@ class Player(db.Model):
     college = db.Column(db.String(100), nullable=False)
 
     # relationships
-    team = relationship('Team', back_populates='players')
+    team = relationship("Team", back_populates="players")
 
-    passing_game_logs = relationship('PassingGameLog', back_populates='player')
-    rushing_game_logs = relationship('RushingGameLog', back_populates='player')
-    receiving_game_logs = relationship('ReceivingGameLog', back_populates='player')
+    passing_game_logs = relationship("PassingGameLog", back_populates="player")
+    rushing_game_logs = relationship("RushingGameLog", back_populates="player")
+    receiving_game_logs = relationship("ReceivingGameLog", back_populates="player")
 
     def __repr__(self):
-        return f'<Team {self.player_name}>'
+        return f"<Team {self.player_name}>"
+
 
 class Game(db.Model):
-    __tablename__ = 'games'
+    __tablename__ = "games"
     game_id = db.Column(db.Integer, primary_key=True)
-    home_team_abbreviation = db.Column(db.String(3), db.ForeignKey('teams.abbreviation'), nullable=False)
-    away_team_abbreviation = db.Column(db.String(3), db.ForeignKey('teams.abbreviation'), nullable=False)
+    home_team_abbreviation = db.Column(
+        db.String(3), db.ForeignKey("teams.abbreviation"), nullable=False
+    )
+    away_team_abbreviation = db.Column(
+        db.String(3), db.ForeignKey("teams.abbreviation"), nullable=False
+    )
     season = db.Column(db.Integer, nullable=False)
     week = db.Column(db.Integer, nullable=False)
     box_score_url = db.Column(db.String(100), nullable=False)
 
     # relationships
-    home_team = relationship('Team', foreign_keys=[home_team_abbreviation], back_populates='home_games')
-    away_team = relationship('Team', foreign_keys=[away_team_abbreviation], back_populates='away_games')
+    home_team = relationship(
+        "Team", foreign_keys=[home_team_abbreviation], back_populates="home_games"
+    )
+    away_team = relationship(
+        "Team", foreign_keys=[away_team_abbreviation], back_populates="away_games"
+    )
 
-    passing_game_logs = relationship('PassingGameLog', back_populates='game')
-    rushing_game_logs = relationship('RushingGameLog', back_populates='game')
-    receiving_game_logs = relationship('ReceivingGameLog', back_populates='game')
+    passing_game_logs = relationship("PassingGameLog", back_populates="game")
+    rushing_game_logs = relationship("RushingGameLog", back_populates="game")
+    receiving_game_logs = relationship("ReceivingGameLog", back_populates="game")
 
     def __repr__(self):
-        return f'<Game {self.home_team_abbreviation} {self.away_team_abbreviation}>'
-    
+        return f"<Game {self.home_team_abbreviation} {self.away_team_abbreviation}>"
+
+
 class PassingGameLog(db.Model):
-    __tablename__ = 'passing_game_logs'
+    __tablename__ = "passing_game_logs"
     passing_log_id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('games.game_id'), nullable=False)
-    player_name = db.Column(db.String(100), db.ForeignKey('players.player_name'), nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey("games.game_id"), nullable=False)
+    player_name = db.Column(
+        db.String(100), db.ForeignKey("players.player_name"), nullable=False
+    )
     completions = db.Column(db.Integer, nullable=False)
     attempts = db.Column(db.Integer, nullable=False)
     yards = db.Column(db.Integer, nullable=False)
@@ -103,28 +123,34 @@ class PassingGameLog(db.Model):
     fantasy_points = db.Column(db.Integer, nullable=False)
 
     # relationships
-    game = relationship('Game', back_populates='passing_game_logs')
-    player = relationship('Player', back_populates='passing_game_logs')
+    game = relationship("Game", back_populates="passing_game_logs")
+    player = relationship("Player", back_populates="passing_game_logs")
+
 
 class RushingGameLog(db.Model):
-    __tablename__ = 'rushing_game_logs'
+    __tablename__ = "rushing_game_logs"
     rushing_log_id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('games.game_id'), nullable=False)
-    player_name = db.Column(db.String(100), db.ForeignKey('players.player_name'), nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey("games.game_id"), nullable=False)
+    player_name = db.Column(
+        db.String(100), db.ForeignKey("players.player_name"), nullable=False
+    )
     carries = db.Column(db.Integer, nullable=False)
     yards = db.Column(db.Integer, nullable=False)
     touchdowns = db.Column(db.Integer, nullable=False)
     fantasy_points = db.Column(db.Integer, nullable=False)
 
     # relationships
-    game = relationship('Game', back_populates='rushing_game_logs')
-    player = relationship('Player', back_populates='rushing_game_logs')
+    game = relationship("Game", back_populates="rushing_game_logs")
+    player = relationship("Player", back_populates="rushing_game_logs")
+
 
 class ReceivingGameLog(db.Model):
-    __tablename__ = 'receiving_game_logs'
+    __tablename__ = "receiving_game_logs"
     receiving_log_id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('games.game_id'), nullable=False)
-    player_name = db.Column(db.String(100), db.ForeignKey('players.player_name'), nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey("games.game_id"), nullable=False)
+    player_name = db.Column(
+        db.String(100), db.ForeignKey("players.player_name"), nullable=False
+    )
     targets = db.Column(db.Integer, nullable=False)
     receptions = db.Column(db.Integer, nullable=False)
     yards = db.Column(db.Integer, nullable=False)
@@ -132,61 +158,84 @@ class ReceivingGameLog(db.Model):
     fantasy_points = db.Column(db.Integer, nullable=False)
 
     # relationships
-    game = relationship('Game', back_populates='receiving_game_logs')
-    player = relationship('Player', back_populates='receiving_game_logs')
+    game = relationship("Game", back_populates="receiving_game_logs")
+    player = relationship("Player", back_populates="receiving_game_logs")
 
-@app.route('/', methods = ['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    if(request.method == 'GET'):
-  
+    if request.method == "GET":
         data = "hello world"
-        return jsonify({'data': data})
-    
-@app.route('/api/roster/<team_abbreviation>', methods=['GET'])
+        return jsonify({"data": data})
+
+
+@app.route("/api/roster/<team_abbreviation>", methods=["GET"])
 def get_team_roster(team_abbreviation):
-    
     team_players = Player.query.filter_by(team_abbreviation=team_abbreviation).all()
 
     # Convert the player objects to a list of dictionaries
     player_list = []
     for player in team_players:
-        player_list.append({
-            'player_id': player.player_id,
-            'player_name': player.player_name,
-            'team_abbreviation': player.team_abbreviation,
-            'position': player.position,
-            'jersey_number': player.jersey_number,
-            'image_url': player.image_url,
-            'height': player.height,
-            'weight': player.weight,
-            'experience': player.experience,
-            'college': player.college
-        })
+        player_list.append(
+            {
+                "player_id": player.player_id,
+                "player_name": player.player_name,
+                "team_abbreviation": player.team_abbreviation,
+                "position": player.position,
+                "jersey_number": player.jersey_number,
+                "image_url": player.image_url,
+                "height": player.height,
+                "weight": player.weight,
+                "experience": player.experience,
+                "college": player.college,
+            }
+        )
 
     return jsonify(player_list)
 
-@app.route('/api/player/<int:player_id>', methods=['GET'])
+
+@app.route("/api/player/<int:player_id>", methods=["GET"])
 def get_player(player_id):
-    
     player = Player.query.filter_by(player_id=player_id).first()
 
     if player:
-        return jsonify({
-            'player_id': player.player_id,
-            'player_name': player.player_name,
-            'team_abbreviation': player.team_abbreviation,
-            'position': player.position,
-            'jersey_number': player.jersey_number,
-            'image_url': player.image_url,
-            'height': player.height,
-            'weight': player.weight,
-            'experience': player.experience,
-            'college': player.college
-        })
+        return jsonify(
+            {
+                "player_id": player.player_id,
+                "player_name": player.player_name,
+                "team_abbreviation": player.team_abbreviation,
+                "position": player.position,
+                "jersey_number": player.jersey_number,
+                "image_url": player.image_url,
+                "height": player.height,
+                "weight": player.weight,
+                "experience": player.experience,
+                "college": player.college,
+            }
+        )
     else:
-        return jsonify({'message': 'Player not found'}, 404)
-  
+        return jsonify({"message": "Player not found"}, 404)
+
+
+@app.route("/api/games", methods=["GET"])
+def get_games():
+    games = Game.query.all()
+    game_list = []
+    for game in games:
+        game_list.append(
+            {
+                "game_id": game.game_id,
+                "home_team_abbreviation": game.home_team_abbreviation,
+                "away_team_abbreviation": game.away_team_abbreviation,
+                "season": game.season,
+                "week": game.week,
+                "box_score_url": game.box_score_url,
+            }
+        )
+
+    return jsonify(game_list)
+
+
 # driver function
-if __name__ == '__main__':
-  
-    app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug=True)
